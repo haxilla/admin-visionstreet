2c20e54ecfed2f833b4adafc96d9f0407c37a1a7
Your Name <you@example.com>
2025-04-13 12:18:17 -0700

    adding trusted proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Request;
-use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,12 +19,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Request::setTrustedProxies(
-            [
-                '23.227.151.74', // IP address of your Apache server
-                '127.0.0.1',     // If Apache and Laravel are on the same host
-            ],
-            Request::HEADER_X_FORWARDED_ALL
-        );
     }
 }
