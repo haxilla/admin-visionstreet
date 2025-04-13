c95edb68e9362f4d3400fc39c17eddae4768bc62
Your Name <you@example.com>
2025-04-13 14:54:09 -0700

    adding trusted proxies

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/_debug-proxy', function () {
+    return [
+        'X-Forwarded-Proto' => request()->header('X-Forwarded-Proto'),
+        'isSecure()' => request()->isSecure(),
+        'url()->current()' => url()->current(),
+        'app_url' => config('app.url'),
+    ];
+});
+
 Route::get('/', function () {
     return view('index');
 });
