680193956174009904ebc60ba2f6f0b6cb795637
Your Name <you@example.com>
2025-04-13 12:01:15 -0700

    adding trusted proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Request::setTrustedProxies(
+            [
+                '23.227.151.74', // IP address of your Apache server
+                '127.0.0.1',     // If Apache and Laravel are on the same host
+            ],
+            Request::HEADER_X_FORWARDED_ALL
+        );
     }
 }
