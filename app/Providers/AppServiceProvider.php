16ff7c7d79766dfb87bf082e212cadf029c16605
Your Name <you@example.com>
2025-04-13 16:08:09 -0700

    adding trusted proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,10 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        /*
+        
         if (env('FORCE_HTTPS', false)) {
             URL::forceScheme('https');
         }
-        */
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('_debug-proxy', function () {
-    return response()->json([
-        'X-Forwarded-Proto' => request()->header('X-Forwarded-Proto'),
-        'isSecure()' => request()->isSecure(),
-        'url()->current()' => url()->current(),
-        'app_url' => config('app.url'),
-    ]);
-})->middleware(\App\Http\Middleware\TrustProxies::class);
-
-
 Route::get('/', function () {
     return view('index');
 });
