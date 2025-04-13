3874ddfc89d435f05c42e09b1ca885afa903f300
Your Name <you@example.com>
2025-04-13 16:02:49 -0700

    adding trusted proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -9,5 +9,10 @@ class TrustProxies extends Middleware
 {
     protected $proxies = '*'; // You can also use ['127.0.0.1'] if you want to be strict
 
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = 
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_PORT;
+
 }
