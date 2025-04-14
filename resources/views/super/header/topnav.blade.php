87d71b5c662b41ad52fe2a60e56aae534884870c
Your Name <you@example.com>
2025-04-14 16:27:56 -0700

    fixing routes

--- resources/views/super/header/topnav.blade.php
+++ resources/views/super/header/topnav.blade.php
@@ -3,7 +3,7 @@
     {{-- Logo with no padding and full height --}}
     <div class="flex items-center px-6 py-2 h-full">
         <a href="/super/dashboard"
-           class="logo-lightning group flex items-center gap-2 text-2xl uppercase font-russo text-sky-400 relative select-none">
+           class="group flex items-center gap-2 text-2xl uppercase font-russo text-sky-300 tracking-wide relative select-none">
             <svg class="w-5 h-5 text-sky-300 group-hover:text-white transition duration-150" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                  viewBox="0 0 24 24">
                 <path d="M13 2L3 14h7v8l11-12h-8z"/>
