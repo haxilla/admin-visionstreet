23f29cdc35aca3e9ff7c569ac8da5e7b9ed09413
Your Name <you@example.com>
2025-04-14 11:41:30 -0700

    fixing routes

--- resources/views/super/dashboard.blade.php
+++ resources/views/super/dashboard.blade.php
@@ -1,4 +1,2 @@
-<x-layout>
-    <h1 class="text-2xl font-bold">Super Dashboard</h1>
-    <p>Welcome, {{ auth()->user()->email }}</p>
-</x-layout>
+<h1 class="text-2xl font-bold">Super Dashboard</h1>
+<p>Welcome, {{ auth()->user()->email }}</p>
