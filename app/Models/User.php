ca5f7f62a5c3ed5979e4637cb1c260a84fdd7d12
Your Name <you@example.com>
2025-04-13 23:55:46 -0700

    fixing

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
--- database/migrations/0001_01_01_000000_create_projects_table.php
+++ database/migrations/0001_01_01_000000_create_projects_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('project_id')->constrained()->onDelete('cascade');
-            $table->string('access_level'); // e.g., 'member' or 'admin'
             $table->timestamps();
             $table->unique(['user_id', 'project_id']);
         });
