e304f2c4550b34c5d66670eeb106e7dde9ea32d2
Your Name <you@example.com>
2025-04-14 10:58:10 -0700

    fixing routes

--- app/Http/Controllers/Guest/GuestController.php
+++ app/Http/Controllers/Guest/GuestController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Http;
 
 class GuestController extends Controller
 {
