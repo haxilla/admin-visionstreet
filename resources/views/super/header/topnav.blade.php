42cdce6b84bfa17af26f9b1a6cdadcd9c7c83d04
Your Name <you@example.com>
2025-04-14 20:03:55 -0700

    fixing routes

--- resources/views/super/header/topnav.blade.php
+++ resources/views/super/header/topnav.blade.php
@@ -19,7 +19,7 @@
           <button
             @click="open = !open"
             @click.away="open = false"
-            class="text-sky-400 hover:text-white transition duration-200 focus:outline-none"
+            class="text-sky-400 hover:text-white transition duration-200 focus:outline-none cursor-pointer"
             aria-label="Settings"
           >
             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 fill-current" viewBox="0 0 24 24">
