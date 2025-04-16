65b8f0d1cd33c7b31d133ef222961229f5ef281d
Your Name <you@example.com>
2025-04-16 12:47:01 -0700

    fixing super dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        sidebar: '#0f172a',
+        accentBlue: '#3b82f6',
+        accentYellow: '#facc15',
+        accentPink: '#f472b6',
+        accentTeal: '#2dd4bf',
+      },
+    },
+  },
+  plugins: [],
+};
