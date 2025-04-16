bb04a40fb8fdb33cbdc3fa8cd27b11afba5c3b56
Your Name <you@example.com>
2025-04-16 12:29:24 -0700

    fixing super dashboard

--- resources/views/super/dashboard.blade.php
+++ resources/views/super/dashboard.blade.php
@@ -1,19 +1,4 @@
-@include('super.header.doctype')
-<script>
-  tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          sidebar: '#0f172a',
-          accentBlue: '#3b82f6',
-          accentYellow: '#facc15',
-          accentPink: '#f472b6',
-          accentTeal: '#2dd4bf',
-        },
-      },
-    },
-  };
-</script>
+
 <body class="bg-white h-full font-sans text-gray-800">
 
   <!-- Layout Container -->
