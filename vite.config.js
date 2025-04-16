1464b7660803a77e6c39c58bd578a19ec2da13e4
Your Name <you@example.com>
2025-04-16 14:59:04 -0700

    fixing super dashboard

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     base: '/build',
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        tailwindcss(),
     ],
 });
