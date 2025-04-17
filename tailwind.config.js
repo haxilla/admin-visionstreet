b30d72e58c2271bc7af43280f0eb27a5937b88a2
Your Name <you@example.com>
2025-04-16 19:14:33 -0700

    synch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
-module.exports = {
+export default {
   content: [
     './resources/views/**/*.blade.php',
     './resources/js/**/*.js',
+    './resources/**/*.php', // optional fallback
   ],
   theme: {
     extend: {
       colors: {
         sidebar: '#0f172a',
-        accentBlue: '#3b82f6',
-        accentYellow: '#facc15',
-        accentPink: '#f472b6',
-        accentTeal: '#2dd4bf',
       },
     },
   },
   plugins: [],
 }
+
 
