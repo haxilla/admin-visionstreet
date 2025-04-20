5604330551bb63f782621e61e7cf666186842692
Your Name <you@example.com>
2025-04-19 23:23:08 -0700

    adding topnav

--- app/Http/Controllers/Client/ClientHandleController.php
+++ app/Http/Controllers/Client/ClientHandleController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\Client;
-
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
 
 class ClientHandleController extends Controller
 {
