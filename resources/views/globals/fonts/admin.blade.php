89f76db9982b5cdfc00deabb5ef494a8c9a421c7
Your Name <you@example.com>
2025-04-21 18:00:44 -0700

    adding client form

---
+++ resources/views/globals/fonts/admin.blade.php
@@ -0,0 +1,21 @@
+<style>
+	@font-face {
+		font-family: 'Russo One';
+		src: url('/fonts/RussoOne-Regular.ttf') format('truetype');
+		font-weight: normal;
+		font-style: normal;}
+
+	.font-russo {
+		font-family: 'Russo One', sans-serif;}
+
+	@font-face {
+	  font-family: 'Orbitron';
+	  src: url('/fonts/Orbitron-VariableFont_wght.ttf') format('truetype');
+	  font-weight: 100 900;
+	  font-display: swap;
+	}
+
+	.font-orbitron {
+	  font-family: 'Orbitron', sans-serif;
+	}
+</style>
