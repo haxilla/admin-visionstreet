89ea2f97c72bef8820f113835b90f9a64f79a358
Your Name <you@example.com>
2025-04-22 20:45:05 -0700

    fixing

--- resources/views/admin/tools/postgres/index.blade.php
+++ resources/views/admin/tools/postgres/index.blade.php
@@ -21,7 +21,7 @@ isOpen(route) { return this.activeDropdown === route; }}">
         </h1>
       </div>
       <div class="pageswap p-6 w-full">
-          @include('admin.tools.postgres.queries.schemas')
+          @include('admin.tools.postgres.)
       </div>
     </div>
   </main>
