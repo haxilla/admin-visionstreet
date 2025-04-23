2472754d0d26c062cf6512949b32d3ce10bce5fd
Your Name <you@example.com>
2025-04-22 20:53:54 -0700

    fixing

--- app/Http/Controllers/Data/PostgresController.php
+++ app/Http/Controllers/Data/PostgresController.php
@@ -15,8 +15,6 @@ class PostgresController extends Controller
 
         include(app_path().'/admin/tools/postgres/schemas/show.php');
 
-        dd('???');
-        
         return view('admin.tools.postgres.index', [
             'schemas' => $schemas,
         ]);
