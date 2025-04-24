41edd79bf88ee19b51024f0afb7abb464b65f79e
Your Name <you@example.com>
2025-04-23 19:45:29 -0700

    changing postgres code

--- resources/views/admin/tools/postgres/index.blade.php
+++ resources/views/admin/tools/postgres/index.blade.php
@@ -21,7 +21,7 @@ isOpen(route) { return this.activeDropdown === route; }}">
         </h1>
       </div>
       <div class="pageswap p-6 w-full">
-        {!! $result !!}
+        dd($data);
       </div>
     </div>
   </main>
