0f4144b8fbb365417ff70bc7780e9326e146271a
Your Name <you@example.com>
2025-04-24 16:06:23 -0700

    fixing postgres tool

--- resources/js/handle.js
+++ resources/js/handle.js
@@ -128,8 +128,6 @@ if (document.body.classList.contains('linkcheck')) {
       const target = document.querySelector(`.${renderTo}`);
       if (target) {
         target.innerHTML = html;
-        //registers dropdown functions
-        dropdownMenus();
       } else {
         console.warn(`⚠️ Target container .${renderTo} not found`);
       }
