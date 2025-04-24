8b2b181015c19c3126e3f6d5374079fa751d4f88
Your Name <you@example.com>
2025-04-24 14:14:57 -0700

    fixing postgres tool

--- resources/views/admin/tools/postgres/output.blade.php
+++ resources/views/admin/tools/postgres/output.blade.php
@@ -112,9 +112,8 @@
          data-renderfrom="admin.tools.postgres.tables.deletetable"
          data-renderas="html"
          data-renderto="pageswap"
-         data-schema="{{ $schema }}"
-         data-table="{{ $table }}"
-      >
+         data-schema="{{$data['schema']}}"
+         data-table="{{$data['table']}}">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
