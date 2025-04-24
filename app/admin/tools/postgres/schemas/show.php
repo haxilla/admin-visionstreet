354477b3d14c7cc77e44f8befb35d9dd820e2ed1
Your Name <you@example.com>
2025-04-23 19:30:40 -0700

    changing postgres code

--- app/admin/tools/postgres/schemas/show.php
+++ app/admin/tools/postgres/schemas/show.php
@@ -1,13 +1,13 @@
 <?php
 
-if(empty($schemas)){
-    dd("error-line10-postgres/schemas/show");}
-
 $schemas = \DB::select("
     SELECT schema_name
     FROM information_schema.schemata
     WHERE schema_name NOT IN ('pg_catalog', 'information_schema')
     ORDER BY schema_name");
+
+if(empty($schemas)){
+    dd("error-line10-postgres/schemas/show");}
 
 dd($data);
 
