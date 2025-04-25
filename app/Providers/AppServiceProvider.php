8c9befbd0104236d13feb88ed6280ebbdf0e0419
Your Name <you@example.com>
2025-04-25 15:38:03 -0700

    adding button to create schema, etc

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,14 +35,11 @@ class AppServiceProvider extends ServiceProvider
             $proto = $request->header('x-forwarded-proto');
 
             if (is_array($proto)) {
-                $proto = $proto[0];
-            }
+                $proto = $proto[0];}
 
-            if (is_array($proto)) {
-                $proto = $proto[0]; // grab the first value if array
-            }
             if ($proto === 'https') {
                 URL::forceScheme('https');
+
             } else {
                 Log::warning('FORCE_HTTPS is enabled, but request is not marked secure.', [
                     'url' => request()->fullUrl(),
