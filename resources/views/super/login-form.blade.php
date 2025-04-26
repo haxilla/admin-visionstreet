1db55ed4e0c328f88ebb4ec38b4af2395493f11e
Your Name <you@example.com>
2025-04-25 17:10:32 -0700

    adding button to create schema, etc

--- resources/views/super/login-form.blade.php
+++ resources/views/super/login-form.blade.php
@@ -66,7 +66,8 @@
 
     <script>
         grecaptcha.ready(function () {
-            grecaptcha.execute('{{ env('RECAPTCHA_SITE_KEY') }}', {action: 'super_login'}).then(function (token) {
+            grecaptcha.execute('{{ config('services.recaptcha.site_key') }}'
+, {action: 'super_login'}).then(function (token) {
                 document.getElementById('recaptchaToken').value = token;
             });
         });
