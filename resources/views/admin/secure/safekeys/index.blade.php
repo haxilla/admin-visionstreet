c220f24ea6e4fe4aa528dbbf5e9c079afb7d457d
Your Name <you@example.com>
2025-04-26 14:33:03 -0700

    form m routes

--- resources/views/admin/secure/safekeys/index.blade.php
+++ resources/views/admin/secure/safekeys/index.blade.php
@@ -15,18 +15,19 @@ isOpen(route) { return this.activeDropdown === route; }}">
   <main class="transition-all duration-300 min-h-screen pt-24 relative"
   :class="collapsed ? 'ml-20' : 'ml-64'">
     <div class="ml-8 mr-8 lg:ml-10 lg:mr-10">
-			<div class="flex items-center justify-between mb-6">
-			    <h1 class="text-3xl font-bold text-white tracking-wide">SAFEKEYS</h1>
+			<div class="flex items-center justify-between mb-8">
+    <h1 class="text-3xl font-bold text-white tracking-wide">SAFEKEYS</h1>
 
-			    <a href="#"
-			        class="inline-flex items-center bg-gradient-to-r from-blue-600 to-cyan-400 hover:from-cyan-400 hover:to-blue-600 text-white font-semibold py-2 px-4 rounded-md shadow-md transition transform hover:scale-105">
-			        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-			            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-			                d="M15 7a2 2 0 11-4 0 2 2 0 014 0zM17 9l4 4m0 0l-4 4m4-4H7" />
-			        </svg>
-			        New Key
-			    </a>
-			</div>
+    <a href="#"
+        class="inline-flex items-center bg-blue-700 hover:bg-blue-800 text-white font-semibold py-2 px-4 rounded-md shadow hover:shadow-lg transition-transform transform hover:scale-105">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M12 4v16m8-8H4" />
+        </svg>
+        Add SafeKey
+    </a>
+</div>
+
       <div class="pageswap p-6 w-full">
         @include('admin.secure.safekeys.output')
       </div>
